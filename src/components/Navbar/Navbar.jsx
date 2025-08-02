a1ebb0a6836c3abbb70bbd6c90b0fb563b133102
neharepos <nehakeshrimail@gmail.com>
2025-08-02 23:19:14 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Navbar = () => {
+  return (
+    <div>
+        <RxHamburgerMenu />
+    </div>
+  )
+}
+
+export default Navbar
