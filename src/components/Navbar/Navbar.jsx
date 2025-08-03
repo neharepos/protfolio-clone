07dbf7be1c13bccc8c04e9cdff94f21fb5706a7e
neharepos <nehakeshrimail@gmail.com>
2025-08-03 22:58:54 +0530

    made the nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
-    <div>
-        <RxHamburgerMenu />
+    <div className='flex justify-end text-white gap-4 mr-10 mt-6 text-lg'>
+      
+       <div className='hidden sm:flex gap-4'>
+        
+        <ul className='flex gap-4'>
+            <li><a href="#" className=' hover:text-blue-600'>Home</a></li>
+            <li><a href="#" className=' hover:text-blue-600'>Blogs</a></li>
+            <li><a href="#" className=' hover:text-blue-600'>Notes</a></li>
+            <li><a href="#" className=' hover:text-blue-600'>Projects</a></li>
+            <li><a href="#" className=' hover:text-blue-600'>About</a></li>
+        </ul> 
+        </div>
+        
+          <div className='inline sm:hidden font-bold cursor-pointer ' onClick={toggleMenu}>
+             {isOpen ? <IoMdClose /> : <RxHamburgerMenu />}
+        
+        </div>
+        {isOpen && (
+        <div className='flex flex-col items-start gap-4 px-4 pb-4 sm:hidden text-lg'>
+          <a href="#" className='hover:text-blue-600'>Home</a>
+          <a href="#" className='hover:text-blue-600'>Blogs</a>
+          <a href="#" className='hover:text-blue-600'>Notes</a>
+          <a href="#" className='hover:text-blue-600'>Projects</a>
+          <a href="#" className='hover:text-blue-600'>About</a>
+        </div>
+        )}
     </div>
+
   )
 }
 
